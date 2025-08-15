90f3bc4f1a7b47b2ffe53341dfde0da9385d8047
Ayub Ali <97268297+yubiszn@users.noreply.github.com>
2025-08-15 15:34:13 -0400

    Updated intro text in hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,9 +20,9 @@ const Hero = () => {
             Hi, I'm <span className='text-blue-500'>Ayub</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            As a passionate software developer in Columbus, Ohio,<br className='sm:block hidden' />
-            I build full-stack web applications and interactive user experiences.<br className='sm:block hidden' />
-            I create scalable, high-impact solutions from eCommerce platforms to award winning projects using React, Node.js, Spring Boot, and MongoDB.
+            As a software developer in Columbus, Ohio,<br className='sm:block hidden' /> I design full-stack applications and interactive experiences,<br className='sm:block hidden' />
+creating scalable, high-impact solutions that drive results.
+
           </p>
         </div>
       </div>
